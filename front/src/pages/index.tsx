9e7611f34fc18a88b15e6540176ba5c71f32093a
Cauê Santos <cauedossantossilva@hotmail.com>
2021-09-19 14:53:53 -0300

    Changes

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -23,7 +23,7 @@ function Home() {
         <MaxContent>
           <div>
             <h1>Sobre nós</h1>
-            <p>Associação de Peritos Judiciais do Estado de Santa Catarina (APJESC), iniciou suas atividades em 24 de novembro do ano de 2020, com sede e foro na Capital, localizada à rua Esteves Júnior, 50 - Sala 404, Edifício Top Tower, Centro, CEP: 88.015-130, Florianópolis/SC. Endereço eletrônico: apjesc@gmail.com, Contato: +55 48  98813-4767. </p>
+            <p>Associação de Peritos Judiciais do Estado de Santa Catarina (APJESC), iniciou suas atividades em 28 de outubro do ano de 2020, com sede e foro na Capital, localizada à rua Esteves Júnior, 50 - Sala 404, Edifício Top Tower, Centro, CEP: 88.015-130, Florianópolis/SC. Endereço eletrônico: apjesc@gmail.com, Contato: +55 48  98813-4767. </p>
             <p>Somos uma associação que nasceu com intuito de representar e valorizar o ofício do perito catarinense de maneira igualitária e apartidária em todas as suas atuações e em caráter especial, junto ao poder judiciário e as outras entidades que necessitem de serviços profissionais nas áreas específicas de cada associado.
               Peritos Especialistas em diversas áreas do conhecimento, ávidos por aprimoramento, e comprometidos com a ética na atuação e em sua representatividade.
               Somos uma congregação de profissionais atuantes na esfera judicial engajados e motivados em normatizar nossa atuação no Estado de Santa Catarina e no Brasil.</p>
